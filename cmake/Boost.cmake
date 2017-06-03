6f913b1883919f0abe0e19838f05b504c6f5c1c6
tokuda109 <tokuda109@gmail.com>
2017-06-03 17:14:16 +0900

    add class template

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -2,11 +2,8 @@
 cmake_minimum_required(VERSION 3.7.1 FATAL_ERROR)
 
 macro(prepare_boost)
-    if(NOT WIN32)
-        set(CMAKE_CXX_FLAGS "-pthread")
-    endif()
 
-    if(USE_STATIC_BOOST)
+    if (USE_STATIC_BOOST)
         set(Boost_USE_STATIC_LIBS ON)
     endif()
 
