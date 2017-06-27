81bb2945fc03f012284b2c82c9ad9e3f84d7f8e1
tokuda109 <tokuda109@gmail.com>
2017-06-27 17:32:26 +0900

    fix test name duplicated

--- src/boost/CTestTestfile.cmake
+++ src/boost/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(unittest "/Users/tokuda109/Documents/cpp/cpp-training/build/boost")
-set_tests_properties(unittest PROPERTIES  WORKING_DIRECTORY "/Users/tokuda109/Documents/cpp/cpp-training/src/boost")
+add_test(boost_test "/Users/tokuda109/Documents/cpp/cpp-training/build/boost")
+set_tests_properties(boost_test PROPERTIES  WORKING_DIRECTORY "/Users/tokuda109/Documents/cpp/cpp-training/src/boost")
--- src/cpp/CTestTestfile.cmake
+++ src/cpp/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(unittest "/Users/tokuda109/Documents/cpp/cpp-training/build/cpp")
-set_tests_properties(unittest PROPERTIES  WORKING_DIRECTORY "/Users/tokuda109/Documents/cpp/cpp-training/src/cpp")
+add_test(cpp_test "/Users/tokuda109/Documents/cpp/cpp-training/build/cpp")
+set_tests_properties(cpp_test PROPERTIES  WORKING_DIRECTORY "/Users/tokuda109/Documents/cpp/cpp-training/src/cpp")
